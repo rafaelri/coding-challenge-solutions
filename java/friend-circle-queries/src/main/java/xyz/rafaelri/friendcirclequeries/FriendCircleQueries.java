33eee2cf3c962c53a0666ae61aee0f28960e6dd2
rafaelri <rafaelri@gmail.com>
2020-05-29 17:18:01 -0300

    weighted-union-find

--- java/friend-circle-queries/src/main/java/xyz/rafaelri/friendcirclequeries/FriendCircleQueries.java
+++ java/friend-circle-queries/src/main/java/xyz/rafaelri/friendcirclequeries/FriendCircleQueries.java
@@ -6,23 +6,64 @@ import java.util.Map;
 public class FriendCircleQueries {
     static int[] maxCircle(int[][] queries) {
         int[] res = new int[queries.length];
-        Map<Integer,Integer> idToCircleSize = new HashMap<Integer,Integer>();
+        Map<Integer,Integer> idToPrimary = new HashMap<Integer,Integer>();
+        Map<Integer,Integer> primaryToSize = new HashMap<Integer,Integer>();
         int max = 0;
         for (int i=0;i<queries.length;i++) {
-            int circleSize = updateCircleSizes(idToCircleSize, queries[i]);
+            int circleSize = updateCircleSizes(idToPrimary, primaryToSize, queries[i]);
             max = Math.max(max, circleSize);
             res[i]=max;
         }
         return res;
     }
-    static int updateCircleSizes(Map<Integer,Integer> idToCircleSize, int[] query) {
+    static int updateCircleSizes(Map<Integer,Integer> idToPrimary, Map<Integer,Integer> primaryToSize, int[] query) {
         int id1 = query[0];
         int id2 = query[1];
-        int size1 = idToCircleSize.getOrDefault(id1, 1);
-        int size2 = idToCircleSize.getOrDefault(id2, 1);
-        int sum = size1+size2;
-        idToCircleSize.put(id1, sum);
-        idToCircleSize.put(id2, sum);
-        return sum;
+        int primary1 = findPrimary(idToPrimary, id1);
+        int size1 = findSize(primaryToSize, primary1);
+        int primary2 = findPrimary(idToPrimary, id2);
+        int size2 = findSize(primaryToSize, primary2);
+        if (primary1 != primary2) {
+            if (size2> size1) {
+                updatePrimary(idToPrimary, primary1, primary2);
+                updateSize(primaryToSize, primary2, size1+size2);
+                return size1+size2;
+            }
+            else {
+                updatePrimary(idToPrimary, primary2, primary1);
+                updateSize(primaryToSize, primary1, size1+size2);
+                return size1+size2;
+            }
+        }
+        else {
+            if (size1>size2)
+                return size1;
+            else
+                return size2;
+        }
     }
+
+    static void updateSize(Map<Integer,Integer> primaryToSize, int id, int size) {
+        primaryToSize.put(id, size);
+    }
+
+    static int findSize(Map<Integer,Integer> primaryToSize, int id) {
+        return primaryToSize.getOrDefault(id, 1);
+    }
+
+
+    static void updatePrimary(Map<Integer,Integer> idToPrimary, int id, int primary) {
+        idToPrimary.put(id, primary);
+    }
+
+    static int findPrimary(Map<Integer,Integer> idToPrimary, int id) {
+        int search = id;
+        int response = idToPrimary.getOrDefault(search, search);
+        while (response != search) {
+            search = response;
+            response = idToPrimary.getOrDefault(search, search);
+        }
+        return response;
+    }
+
 }
--- java/friend-circle-queries/src/test/java/xyz/rafaelri/friendcirclequeries/TestFriendCircleQueries.java
+++ java/friend-circle-queries/src/test/java/xyz/rafaelri/friendcirclequeries/TestFriendCircleQueries.java
@@ -18,4 +18,14 @@ public class TestFriendCircleQueries {
         assertThat(FriendCircleQueries.maxCircle(new int[][]{new int[]{1, 2}, new int[]{3, 4},new int[]{1, 4}}), equalTo(new int[]{2, 2, 4}));
     }
 
+    @Test
+    public void testFourConnectionsDistinctBounds() {
+        assertThat(FriendCircleQueries.maxCircle(new int[][]{new int[]{1, 2}, new int[]{3, 4},new int[]{1, 4}, new int[]{5,6}, new int[]{3,6}}), equalTo(new int[]{2, 2, 4, 4, 8}));
+    }
+
+    @Test
+    public void testTwoConnectionsThreeElements() {
+        assertThat(FriendCircleQueries.maxCircle(new int[][]{new int[]{1, 2}, new int[]{1, 3}}), equalTo(new int[]{2, 3}));
+    }
+
 }
