25ee1e505bda472bda8fdcd7ef203e3ffc7cac4e
rafaelri <rafaelri@gmail.com>
2020-05-28 23:43:36 -0300

    dynamic programming

--- java/david-staircase/src/main/java/xyz/rafaelri/davidstaircase/DavidStaircase.java
+++ java/david-staircase/src/main/java/xyz/rafaelri/davidstaircase/DavidStaircase.java
@@ -1,15 +1,23 @@
 package xyz.rafaelri.davidstaircase;
 
 public class DavidStaircase {
-    public static int stepPermsInt(int n) {
-        switch (n) {
-            case 1: return 1;
-            case 2: return 2;
-            case 3: return 4;
-            default: return stepPermsInt(n-1) + stepPermsInt(n-2) + stepPermsInt(n-3);
-        }
-    }
     public static int stepPerms(int n) {
-        return (int) ((long) stepPermsInt(n) % 10000000007l);
+        switch(n) {
+            case 1:
+            return 1;
+            case 2:
+            return 2;
+            case 3: 
+            return 4;
+            default:
+                int[] res = new int[n+1];
+                res[1] = 1;
+                res[2] = 2;
+                res[3] = 4;
+                for (int i=4;i<=n;i++) {
+                  res[i]=res[i-1]+res[i-2]+res[i-3];
+             }
+             return (int) (res[n] % 10000000007l);
+        }
     }
 }
